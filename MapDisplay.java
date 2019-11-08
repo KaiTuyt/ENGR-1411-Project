5f9658df155172f47b08a16c00b97ede71614ec8
KaiTuyt <jay.w.shoumaker-1@ou.edu>
2019-11-07 20:03:27 -0600

    Added a box within the box in MapDisplay

--- MapDisplay.java
+++ MapDisplay.java
@@ -21,18 +21,29 @@ public class MapDisplay extends JFrame{
 	private static final long serialVersionUID = 1L;
 	
 	JPanel panel0 = new JPanel();
+	JPanel panel1 = new JPanel();
 	JButton button = new JButton("Calculate Distance");
 	JTextField firstBox = new JTextField(5);
 	JTextField secondBox = new JTextField(5);
 	SpinnerNumberModel spinnerModel = new SpinnerNumberModel(10, 0, 100, 1);
 	JSpinner spinner = new JSpinner(spinnerModel);
+	JTextField title = new JTextField("Map Program");
 	
-	public MapDisplay(String title) {
-		super(title);
+	public MapDisplay(String header) {
+		super(header);
 		setSize(400, 400);
 		setVisible(true);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new GridLayout(5, 0));
+		title.setEditable(false);
+		panel0.add(title);
+		add(panel0);
+		firstBox.setEditable(false);
+		firstBox.setText("Insert the maximum distance each person should travel:");
+		panel1.add(firstBox);
+		panel1.add(spinner);
+		add(panel1);
 	}
 }
