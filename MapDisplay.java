d650cd41dc2d3e7c2e5aa9f90c6000029635791e
KaiTuyt <jay.w.shoumaker-1@ou.edu>
2019-11-11 12:38:20 -0600

    Changed box to include button

--- MapDisplay.java
+++ MapDisplay.java
@@ -1,6 +1,8 @@
 import java.awt.GridLayout;
+import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.Hashtable;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -10,26 +12,36 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSlider;
 import javax.swing.JSpinner;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class MapDisplay extends JFrame{
+
+public class MapDisplay extends JFrame implements ActionListener{
 	
+	private static final int AMOUNT_OF_LOCATIONS = 8;
+
 	// TODO: Finish implementing this...
 	private static final long serialVersionUID = 1L;
 	
 	JPanel panel0 = new JPanel();
 	JPanel panel1 = new JPanel();
-	JButton button = new JButton("Calculate Distance");
-	JTextField firstBox = new JTextField(5);
-	JTextField secondBox = new JTextField(5);
-	SpinnerNumberModel spinnerModel = new SpinnerNumberModel(10, 0, 100, 1);
+	JPanel panel2 = new JPanel();
+	JPanel panel3 = new JPanel();
+	JPanel panel4 = new JPanel();
+	JButton nextButton = new JButton("Next ->");
+	JButton calcuclatebutton = new JButton("Calculate Distance");
+	JTextField firstBox = new JTextField();
+	JTextField secondBox = new JTextField();
+	SpinnerNumberModel spinnerModel = new SpinnerNumberModel(3, 1, 10, 1);
 	JSpinner spinner = new JSpinner(spinnerModel);
-	JTextField title = new JTextField("Map Program");
+	JLabel title = new JLabel("Meeting Finder");
+	HashMap<String, Point> locations = new HashMap<>();
 	
 	public MapDisplay(String header) {
+		
 		super(header);
 		setSize(400, 400);
 		setVisible(true);
@@ -37,13 +49,29 @@ public class MapDisplay extends JFrame{
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new GridLayout(5, 0));
-		title.setEditable(false);
+		
 		panel0.add(title);
 		add(panel0);
+		
 		firstBox.setEditable(false);
-		firstBox.setText("Insert the maximum distance each person should travel:");
+		firstBox.setText("Insert the number of people:");
 		panel1.add(firstBox);
 		panel1.add(spinner);
 		add(panel1);
+		
+		nextButton.addActionListener(this);
+		panel2.add(nextButton);
+		add(panel2);
+		
+		
+		
 	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
